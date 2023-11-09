c735f96cc95850168cebc370e1693daf30a068e4
Sami Alhamad <samialhamad80@gmail.com>
2023-11-08 17:57:57 -0800

    Moved index page JavaScript to index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+var slideIndex = 0;
+
+window.onload = function() {
+    showSlides();
+}
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active-dot", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active-dot";
+    setTimeout(showSlides, 2000); // Change slide every 2 seconds
+}
