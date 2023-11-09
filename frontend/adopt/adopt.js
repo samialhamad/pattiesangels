7a389e096f3372edf971c9a61888d2959bbe7ca9
Sami Alhamad <samialhamad80@gmail.com>
2023-11-08 17:40:51 -0800

    Moved each page into it's own folder, created libraries folder

---
+++ frontend/adopt/adopt.js
@@ -0,0 +1,21 @@
+let dogs = [
+    new Dog(
+        0,
+        "German Shepard",
+        "German Shepard puppy rescued recently",
+        "Male",
+        "Bailey.webp",
+        "Bailey",
+        false
+    )
+];
+
+window.onload = function() {
+    console.log("Dogs: " + dogs);
+
+    console.log("Dog Name: " + dogs[0].ageString);
+}
+
+function addDogCards() {
+
+}
