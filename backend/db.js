e6118f4615e71b90f6e3a9de5973b729426fd10f
RaymondH2OS <raymondh2os@gmail.com>
2024-02-10 13:46:18 -0800

    Updated db.js

--- backend/db.js
+++ backend/db.js
@@ -1,49 +1,20 @@
-// const mysql = require('mysql');
-// require('dotenv').config();
-
-// const dbConfig = {
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE
-// };
-
-// const connection = mysql.createConnection(dbConfig);
-
-// connection.connect(error => {
-//   if (error) throw error;
-//   console.log("Successfully connected to the database.");
-// });
-
-// module.exports = connection;
-
-
 const mysql = require('mysql');
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
-});
+};
+
+const connection = mysql.createConnection(dbConfig);
 
 connection.connect(error => {
-  if (error) {
-    console.error('Error connecting to the database:', error);
-    return;
-  }
+  if (error) throw error;
   console.log("Successfully connected to the database.");
-
-  // Test query
-  connection.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-  });
-
-  // End the connection
-  connection.end();
 });
 
-// Export the connection for other modules to use
 module.exports = connection;
+
+
