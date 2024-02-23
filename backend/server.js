952e344894245fa426db6f4785db4e3e8dc00928
RaymondH2OS <raymondh2os@gmail.com>
2024-02-23 14:46:01 -0800

    Made adjustments to server.js for testing on Heroku

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,9 @@ app.use(express.json()); // Middleware for parsing JSON bodies
 app.use(cors()); // Enable CORS for all routes and origins
 app.use('/api/animals', animalRoutes); // Mount the animal routes on the /api/animals path
 
+// Serve static files from the 'frontend' directory
+app.use(express.static('frontend'));
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
