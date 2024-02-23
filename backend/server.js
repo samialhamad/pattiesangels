f470e7b76c4821cedd77e6446af94b0466ce1e69
RaymondH2OS <raymondh2os@gmail.com>
2024-02-23 15:36:09 -0800

    Updated server.js to work with updated Procfile

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const path = require('path');
 const animalRoutes = require('./routes'); // Import the routes for the Animals
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.use(express.json()); // Middleware for parsing JSON bodies
 app.use(cors()); // Enable CORS for all routes and origins
-app.use('/api/animals', animalRoutes); // Mount the animal routes on the /api/animals path
 
 // Serve static files from the 'frontend' directory
-app.use(express.static('frontend'));
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+// Specific route for serving the homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/home', 'index.html'));
+});
+
+// Mount the animal routes on the '/api/animals' path
+app.use('/api/animals', animalRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
