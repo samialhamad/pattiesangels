d86b14b72c42a3e8c53140a5ba586fbbd534e4c6
RaymondH2OS <raymondh2os@gmail.com>
2024-02-23 13:43:56 -0800

    Testing route on Heroku

--- backend/routes.js
+++ backend/routes.js
@@ -1,16 +1,26 @@
-const express = require('express');
-const db = require('./db'); // Import the database connection
+// const express = require('express');
+// const db = require('./db'); // Import the database connection
 
+// const router = express.Router();
+
+// // Route to get all animals
+// router.get('/', (req, res) => {
+//   db.query('SELECT * FROM Animals', (error, results) => {
+//     if (error) {
+//       return res.status(500).json({ error: error.message });
+//     }
+//     res.json(results); // Send the results as JSON
+//   });
+// });
+
+// module.exports = router;
+
+const express = require('express');
 const router = express.Router();
 
-// Route to get all animals
+// Test route
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM Animals', (error, results) => {
-    if (error) {
-      return res.status(500).json({ error: error.message });
-    }
-    res.json(results); // Send the results as JSON
-  });
+    res.json({ message: "Test route is working!" });
 });
 
 module.exports = router;
