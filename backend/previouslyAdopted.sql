31019ce435e364f29701d9a695628d7cd154bd51
Khadeeja Baqui <khadeejabaqui@csus.edu>
2024-02-24 17:33:26 -0800

    Fixing DT-53

--- backend/previouslyAdopted.sql
+++ backend/previouslyAdopted.sql
@@ -1,4 +1,4 @@
-SELECT name, Photo_ID from Animals
+SELECT name, Photo_ID, Testimonial_ID, Author, Content, Date, Image, Testimonials.Animal_ID from Animals
 INNER JOIN AnimalPhotos
 ON Animals.Animal_ID = AnimalPhotos.Animal_ID
 INNER JOIN Testimonials
