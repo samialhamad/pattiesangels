f81888f7e23e5e20c3f9b95a5f44b3cbd6bf0a3e
dinhphucv <90453629+dinhphucv@users.noreply.github.com>
2024-02-24 22:43:20 -0800

    Improvements for applications table, webform / DT-60

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const path = require('path');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session');
+
 const animalRoutes = require('./routes'); // Import the routes for the Animals
+const applyingRoutes = require('./routes/applyingRoutes');
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -10,6 +14,8 @@ const app = express();
 
 app.use(express.json()); // Middleware for parsing JSON bodies
 app.use(cors()); // Enable CORS for all routes and origins
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 // Serve static files from the 'frontend' directory
 app.use(express.static(path.join(__dirname, '../frontend')));
@@ -21,6 +27,8 @@ app.get('/', (req, res) => {
 
 // Mount the animal routes on the '/api/animals' path
 app.use('/api/animals', animalRoutes);
+
+app.use('/apply', applyingRoutes);
 
 const PORT = process.env.PORT || 3000;
 
