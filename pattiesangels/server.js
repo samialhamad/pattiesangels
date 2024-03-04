d8c9eef4b5df3032dd8c2bf7b5281fafd0240bce
dinhphucv <90453629+dinhphucv@users.noreply.github.com>
2024-03-03 21:36:49 -0800

    Remove unnecessary files
    This commit only to cleanup the repo.

--- pattiesangels/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.set('view engine', 'ejs');
-
-app.listen(PORT, () => {
-    console.log("Server listening on PORT", PORT);
-});
