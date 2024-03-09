9471bf075434a2a335d1761bafb63e0ed8418c9d
Selvetta <fmorenomendez@csus.edu>
2024-03-08 21:39:54 -0800

    Removed POST from server.js and mariadb packages

--- backend/db.js
+++ backend/db.js
@@ -10,6 +10,7 @@ const dbConfig = {
 
 const connection = mysql.createConnection(dbConfig);
 
+
 connection.connect(error => {
   if (error) throw error;
   console.log("Successfully connected to the database.");
