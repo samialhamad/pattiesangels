fe5cd1e8cb34d83c43a52b1321f4b621c22b0c35
Sami Alhamad <samialhamad80@gmail.com>
2024-04-03 23:13:25 -0700

    Added initial unit tests for Animal model

---
+++ frontend/model/animal/animaltests.js
@@ -0,0 +1,61 @@
+//ageString()
+
+testAgeString0Months();
+function testAgeString0Months() {
+    var animal = new Animal();
+    animal.ageInMonths = 0;
+
+    var actual = animal.ageString;
+    var expected = "0 months old"
+    assert(actual, expected, arguments.callee.name);
+}
+
+testAgeString1Month();
+function testAgeString1Month() {
+    var animal = new Animal();
+    animal.ageInMonths = 1;
+
+    var actual = animal.ageString;
+    var expected = "1 month old"
+    assert(actual, expected, arguments.callee.name);
+}
+
+testAgeString4Months();
+function testAgeString4Months() {
+    var animal = new Animal();
+    animal.ageInMonths = 4;
+
+    var actual = animal.ageString;
+    var expected = "4 months old"
+    assert(actual, expected, arguments.callee.name);
+}
+
+testAgeString13Months();
+function testAgeString13Months() {
+    var animal = new Animal();
+    animal.ageInMonths = 13;
+
+    var actual = animal.ageString;
+    var expected = "1 year old"
+    assert(actual, expected, arguments.callee.name);
+}
+
+testAgeString1YearOld();
+function testAgeString1YearOld() {
+    var animal = new Animal();
+    animal.ageInMonths = 12;
+
+    var actual = animal.ageString;
+    var expected = "1 year old"
+    assert(actual, expected, arguments.callee.name);
+}
+
+testAgeString12YearsOld();
+function testAgeString12YearsOld() {
+    var animal = new Animal();
+    animal.ageInMonths = 144;
+
+    var actual = animal.ageString;
+    var expected = "12 years old"
+    assert(actual, expected, arguments.callee.name);
+}
