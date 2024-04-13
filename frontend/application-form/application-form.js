1426c9ead4c999fe090e2129e0de1eeff36fd119
dinhphucv <90453629+dinhphucv@users.noreply.github.com>
2024-04-13 15:28:54 -0700

    Further improments / DT-80
    Add timestamp when submission and "User agree to terms and rules when submit the application".

--- frontend/application-form/application-form.js
+++ frontend/application-form/application-form.js
@@ -3,32 +3,32 @@ window.onload = function () {
 }
 
 function setMinMaxDate() {
-    var currentDate = new Date();
-    var minDate = new Date;
+    let currentDate = new Date();
+    let minDate = new Date;
     minDate.setFullYear(currentDate.getFullYear() - 100);
 
-    var year = currentDate.getFullYear();
-    var month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    var day = currentDate.getDate().toString().padStart(2, '0');
-    var formattedCurrentDate = year + '-' + month + '-' + day;
+    let year = currentDate.getFullYear();
+    let month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    let day = currentDate.getDate().toString().padStart(2, '0');
+    let formattedCurrentDate = year + '-' + month + '-' + day;
 
     year = minDate.getFullYear();
     month = (minDate.getMonth() + 1).toString().padStart(2, '0');
     day = minDate.getDate().toString().padStart(2, '0');
-    var formattedMinDate = year + '-' + month + '-' + day;
+    let formattedMinDate = year + '-' + month + '-' + day;
 
     document.getElementById('applicant-dob').setAttribute('min', formattedMinDate);
     document.getElementById('applicant-dob').setAttribute('max', formattedCurrentDate);
 }
 
 function formValidation() {
-    var dobInput = document.getElementById('applicant-dob');
-    var dobValue = dobInput.value;
-    var dobDate = new Date(dobValue);
+    let dobInput = document.getElementById('applicant-dob');
+    let dobValue = dobInput.value;
+    let dobDate = new Date(dobValue);
 
-    var today = new Date();
-    var age = today.getFullYear() - dobDate.getFullYear();
-    var monthDiff = today.getMonth() - dobDate.getMonth();
+    let today = new Date();
+    let age = today.getFullYear() - dobDate.getFullYear();
+    let monthDiff = today.getMonth() - dobDate.getMonth();
 
     if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dobDate.getDate())) {
         age--;
@@ -38,6 +38,8 @@ function formValidation() {
         alert("You must be at least 18 years old to submit the form.");
         return false;
     }
+
+    document.getElementById('submission-timestamp').value = today.toISOString().slice(0, 19).replace('T', ' ');
 
     return true;
 }
