4045a6bc267a845f78144850634b5597178ce76e
Snakey <spysnakey@gmail.com>
2024-04-14 12:58:08 -0700

    updating testimonials

--- frontend/testimonials/testimonials.js
+++ frontend/testimonials/testimonials.js
@@ -5,6 +5,91 @@ window.onload = function() {
 }
 
 function getTestimonials() {
+    var url = 'http://localhost:3000/api/testimonials'; //No api created yet
+
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+            if (request.status == 200) {
+                console.log("GetTestimonials Response: " + request.response);
+
+                var response = JSON.parse(request.response);
+                
+                testimonials = []; // Clear the existing testimonials array
+                for (var i = 0; i < response.length; i++) {
+                    var testimonial = new Testimonial(response[i]);
+                    testimonials.push(testimonial);
+                }
+                addTestimonialDivs();
+            }
+            else {
+                console.error('Error fetching testimonials:', request.statusText);
+            }
+        }
+    }
+    request.send();
+}
+
+function addTestimonialDivs() {
+    var testimonialsContainerDiv = document.getElementById("testimonialsContainerDiv");
+    console.log("Adding testimonial divs...");
+    for (var i = 0; i < testimonials.length; i++) {
+        var testimonial = testimonials[i];
+
+        var testimonialDiv = document.createElement("div");
+        testimonialDiv.setAttribute("class", "testimonialDiv");
+
+        var authorP = document.createElement("p");
+        authorP.innerHTML = "<strong>Author:</strong> " + testimonial.author;
+        testimonialDiv.append(authorP);
+
+        var contentP = document.createElement("p");
+        contentP.innerHTML = "<strong>Testimonial:</strong> " + testimonial.content;
+        testimonialDiv.append(contentP);
+
+        testimonialsContainerDiv.append(testimonialDiv);
+    }
+}
+
+// Testimonial Class
+function Testimonial(data) {
+    this.author = data.author || "";
+    this.content = data.content || "";
+}
+
+// Add new testimonial
+document.getElementById("submitTestimonial").addEventListener("click", function() {
+    var author = document.getElementById("testimonialAuthor").value;
+    var content = document.getElementById("testimonialContent").value;
+
+    var newTestimonial = new Testimonial({ author: author, content: content });
+    testimonials.push(newTestimonial);
+
+    // Clear input fields
+    document.getElementById("testimonialAuthor").value = "";
+    document.getElementById("testimonialContent").value = "";
+
+    addTestimonialDivs(); // Refresh the displayed testimonials
+
+    sendNewTestimonialToBackend(newTestimonial);
+});
+
+function sendNewTestimonialToBackend(testimonial) {
+    // Simulated backend POST request to save the new testimonial
+    console.log("Sending new testimonial to the backend:", testimonial);
+    // Implement the actual HTTP request to your API endpoint here
+}
+
+
+/*var testimonials = [];
+
+window.onload = function() {
+    getTestimonials();
+}
+
+function getTestimonials() {
     var url = 'http://localhost:3000/api/testimonials';
 
     var request = new XMLHttpRequest();
@@ -51,7 +136,7 @@ function addTestimonialDivs() {
         // testimonialDiv.append(img);
 
         var h2 = document.createElement("h2");
-        h2.innerHTML = testimonial.aurthor;
+        h2.innerHTML = testimonial.author;
         testimonialDiv.append(h2);
 
         var date = document.createElement("p");
@@ -70,5 +155,5 @@ function addTestimonialDivs() {
         testimonialsContainerDiv.append(testimonialDiv);
     }
 }
-
+*/
 
