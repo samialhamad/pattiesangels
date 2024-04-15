1955d0f95fd703e187d802198d56aee8ad05c9c1
Khadeeja Baqui <khadeejabaqui@csus.edu>
2024-04-14 21:22:03 -0700

    DT-82

--- frontend/admin/adminhome/admin.php
+++ frontend/admin/adminhome/admin.php
@@ -1,8 +1,15 @@
 <?php
-    if($logged_in) {
-        include_once('addanimal.html');
-        include_once('editanimals.html');
-        include_once('viewanimals.html');
-        include_once('viewforms.html');
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username =='Admin' and $password =='SeniorProj') {
+        header('addanimal.html');
+        header('editanimals.html');
+        header('viewanimals.html');
+        header('viewforms.html');
+        exit;
+    }
+    else {
+        echo 'You are not the admin';
     }
 ?>
