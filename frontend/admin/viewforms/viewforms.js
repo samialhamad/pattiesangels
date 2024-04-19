db2fff780983491c759aea5980a685f2b2dcbe74
RaymondH2OS <47959240+RaymondH2OS@users.noreply.github.com>
2024-04-19 15:09:10 -0700

    Updated GET url

--- frontend/admin/viewforms/viewforms.js
+++ frontend/admin/viewforms/viewforms.js
@@ -6,7 +6,7 @@ window.onload = function() {
 
 function getForms() {
     // var url = 'http://localhost:3000/apply/adoption_forms'; // Testing url
-    var url = 'https://patties-angels-8cd06741a91a.herokuapp.com/apply/all';
+    var url = 'https://patties-angels-8cd06741a91a.herokuapp.com/apply/adoption_forms';
     var request = new XMLHttpRequest();
     request.open("GET", url);
     request.setRequestHeader("Content-Type", "application/json");
